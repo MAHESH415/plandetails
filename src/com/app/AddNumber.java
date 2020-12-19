58ac3385cb7138d7d27774b266ffb23d8aeee26d
SOMAPANGU MAHESH <SOMAPANGU MAHESH@DESKTOP-5D2LK28>
2020-12-19 14:16:13 +0530

    first changes

---
+++ src/com/app/AddNumber.java
@@ -0,0 +1,15 @@
+package com.app;
+
+import java.util.Scanner;
+
+public class AddNumber {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter  first number");
+		Integer a=sc.nextInt();
+		System.out.println("enter second number");
+		Integer b=sc.nextInt();
+	System.out.println("total    "+(a+b));
+
+	}
+}
